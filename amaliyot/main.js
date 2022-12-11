a7ec448a066fab1081ac7f4a0c8f0abf7f09536e
bexzodxamdamov <bexzodxamdamov5@gmail.com>
2022-12-11 19:35:40 +0500

    first commit

---
+++ amaliyot/main.js
@@ -0,0 +1,32 @@
+// object ==========
+
+// let obj = {
+//   age: 11,
+//   job: "fire fighter",
+//   car: "bugatti",
+//   jet: "tu-144",
+// };
+
+// let obj2 = {
+//   age: 11,
+//   job: "fire fighter",
+//   car: "bugatti",
+//   jet: "tu-144",
+//   insof: "yo'q",
+// };
+// console.log(obj);
+
+// object ni oberadigan misol ==========
+// let products = [{ pName: "hp" }, { pName: "mac" }, { pName: "lenovo" }];
+
+// console.log(products[2], products[1], products[0]);
+
+// infinity son chqaradigan misol ==========
+// let num = prompt("son kiritin");
+// let a = 0;
+// let web = "acer predator triton 300se";
+
+// while (a < num) {
+//   console.log(web);
+//   a++;
+// }
